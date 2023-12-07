b487df6fbb1d5c9deb65b4f02e9459a7ddcc4a08
BADINI Rachid Rodrigue <bsrodrigue@gmail.com>
2023-12-07 22:45:10 +0000

    feat(controls): move food with mouse

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <cstdint>
 #include <raylib.h>
 #include <stdlib.h>
@@ -12,9 +13,9 @@
 Color create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 // BASE COLORS
-#define RED_COLOR() create_color(255, 0, 0, 0)
-#define GREEN_COLOR() create_color(0, 255, 0, 0)
-#define BLUE_COLOR() create_color(0, 0, 255, 0)
+#define RED_COLOR() create_color(255, 0, 0, 255)
+#define GREEN_COLOR() create_color(0, 255, 0, 255)
+#define BLUE_COLOR() create_color(0, 0, 255, 255)
 
 // GAME COLORS
 #define PRIMARY_COLOR() create_color(173, 204, 96, 255)
@@ -26,16 +27,32 @@ Color create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 using namespace std;
 
+int get_random_pos() {
+  int pos = GetRandomValue(0, CELL_COUNT);
+  return pos;
+}
+
 void draw_cell(int x, int y, Color color) {
   // TODO: Make this look better
   DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, color);
 }
 
 void draw_rand_cell(Color color) {
-  int x = GetRandomValue(0, WINDOW_HEIGHT - CELL_SIZE);
-  int y = GetRandomValue(0, WINDOW_HEIGHT - CELL_SIZE);
+  int x = get_random_pos();
+  int y = get_random_pos();
 
   draw_cell(x, y, color);
+}
+
+Vector2 get_random_vec2() {
+  Vector2 vec;
+  int x = get_random_pos();
+  int y = get_random_pos();
+
+  vec.x = x;
+  vec.y = y;
+
+  return vec;
 }
 
 Color create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
@@ -46,17 +63,28 @@ Color create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
 class Food {
 
 public:
-  Vector2 position = {5, 6};
+  Vector2 position;
 
-  Food() {}
+  Food() { position = get_random_vec2(); }
 
   void draw() { draw_cell(position.x, position.y, SECONDARY_COLOR()); }
 };
 
+void sync_pos(Vector2 *dest, Vector2 origin) {
+  dest->x = origin.x / CELL_COUNT;
+  dest->y = origin.y / CELL_COUNT;
+}
+
 int main(int argc, char *argv[]) {
   const char *window_title = "Rapid Snake";
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, window_title);
-  SetRandomSeed(RAND_SEED);
+
+  auto time = std::chrono::system_clock::now();
+  auto duration = time.time_since_epoch();
+  auto millis =
+      std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+  SetRandomSeed(RAND_SEED * millis);
+
   SetTargetFPS(FPS);
 
   Food food = Food();
@@ -66,10 +94,9 @@ int main(int argc, char *argv[]) {
 
     ClearBackground(PRIMARY_COLOR());
 
-    // Place game elements
-    food.draw();
+    sync_pos(&food.position, GetMousePosition());
 
-    draw_rand_cell(RED_COLOR());
+    food.draw();
 
     EndDrawing();
   }
